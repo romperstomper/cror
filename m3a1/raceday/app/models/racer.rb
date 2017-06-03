3922c47f837a68f73fda3efa936040a7aef234a7
gary boland <garyboland@gmail.com>
2017-06-04 00:30:46 +0100

    crud rq01 complete

--- m3a1/raceday/app/models/racer.rb
+++ m3a1/raceday/app/models/racer.rb
@@ -4,6 +4,10 @@ class Racer
     @@client = Mongoid::Clients.default
   end
   def self.collection
-    @@client['racers']
+    mongo_client['racers']
+  end
+  def self.all(prototype={},sort={:number => 1},offset=0,limit=nil)
+    limit = 0 unless limit
+    collection.find(prototype).sort(sort).skip(offset).limit(limit)
   end
 end
