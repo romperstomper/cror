5e2097a16a5c2ef3422cddfd8c202450b3fc6432
gary boland <garyboland@gmail.com>
2017-06-04 20:17:32 +0100

    crud rq04 complete

--- m3a1/raceday/app/models/racer.rb
+++ m3a1/raceday/app/models/racer.rb
@@ -1,4 +1,5 @@
 class Racer
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
   def self.mongo_client
     #mdb = 'mongodb://localhost:27017'
     @@client = Mongoid::Clients.default
@@ -9,5 +10,19 @@ class Racer
   def self.all(prototype={},sort={:number => 1},offset=0,limit=nil)
     limit = 0 unless limit
     collection.find(prototype).sort(sort).skip(offset).limit(limit)
+  end
+  def initialize(params={})
+    @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs].to_i
+  end
+  def self.find(id)
+    id = BSON::ObjectId(id) unless id.class == BSON::ObjectId
+    result = all({:_id => id}).map {|r| r}.pop
+    return result.nil? ? nil : Racer.new(result)
   end
 end
